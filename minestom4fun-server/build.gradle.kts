9fd2d7d6884250526d2a078f2acbe40af6b6e76a
Kanelucky <tuankietdoan46@gmail.com>
2026-02-07 14:15:57 +0700

    Charge jar's name

--- minestom4fun-server/build.gradle.kts
+++ minestom4fun-server/build.gradle.kts
@@ -1,62 +1,82 @@
+import org.gradle.kotlin.dsl.support.serviceOf
+import java.io.ByteArrayOutputStream
+
 plugins {
     kotlin("jvm") version "2.3.0"
-    id("com.gradleup.shadow") version "9.3.1"
     kotlin("plugin.serialization") version "2.3.0"
+    id("com.gradleup.shadow") version "9.3.1"
 }
 
 group = "org.kanelucky"
-version = "0.1.2"
+version = "0.1.3"
 
+fun getShortGitHash(): String {
+    val execOperations = project.serviceOf<ExecOperations>()
+    val stdout = ByteArrayOutputStream()
+
+    execOperations.exec {
+        commandLine("git", "rev-parse", "--short", "HEAD")
+        standardOutput = stdout
+        isIgnoreExitValue = true
+    }
+
+    return stdout.toString().trim().ifEmpty { "nogit" }
+}
 
 dependencies {
     testImplementation(kotlin("test"))
 
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.5.24")
-
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-
     implementation("org.snakeyaml:snakeyaml-engine:2.7")
 
     implementation("net.minestom:minestom:2026.01.08-1.21.11")
     implementation("io.github.togar2:MinestomPvP:2025.12.29-1.21.11")
     implementation("ca.atlasengine:atlas-projectiles:2.1.5")
     implementation("dev.hollowcube:polar:1.15.0")
+    implementation("dev.hollowcube:schem:2.0.1")
+    implementation("dev.rollczi:litecommands-minestom:3.10.9")
 
     compileOnly("net.kyori:adventure-api:4.26.1")
-
-    implementation("dev.hollowcube:schem:2.0.1")
-
-    implementation("dev.rollczi:litecommands-minestom:3.10.9")
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(25))
     }
-    withJavadocJar()
 }
 
 kotlin {
     jvmToolchain(25)
 }
 
-
 tasks {
+
     jar {
         manifest {
-            attributes["Main-Class"] = "org.kanelucky.server.Minestom4fun"
-            attributes["Implementation-Version"] = project.version
-            attributes["Implementation-Title"] = "Minestom4fun"
+            attributes(
+                "Main-Class" to "org.kanelucky.server.Minestom4fun",
+                "Implementation-Title" to "Minestom4fun",
+                "Implementation-Version" to project.version,
+                "Git-Commit" to getShortGitHash()
+            )
         }
+    }
+
+    shadowJar {
+        mergeServiceFiles()
+        archiveFileName.set(
+            "minestom4fun-server-${project.version}-${getShortGitHash()}-shaded.jar"
+        )
     }
 
     build {
         dependsOn(shadowJar)
     }
-    shadowJar {
-        mergeServiceFiles()
-        archiveClassifier.set("")
+
+    test {
+        useJUnitPlatform()
     }
 }
 
@@ -67,14 +87,6 @@ tasks.register<JavaExec>("runServer") {
     mainClass.set("org.kanelucky.server.Minestom4fun")
     classpath = sourceSets["main"].runtimeClasspath
 
-    jvmArgs(
-        "-Xms1G",
-        "-Xmx2G"
-    )
-
+    jvmArgs("-Xms1G", "-Xmx2G")
     workingDir = file("run")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }

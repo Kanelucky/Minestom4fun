949efbaacca1422c217ee37ca7049e77bcd4e112
Kanelucky <tuankietdoan46@gmail.com>
2026-02-21 20:43:28 +0700

    Add script

---
+++ scripts/start.bat
@@ -0,0 +1,32 @@
+@echo off
+TITLE Minestom4fun - A Minecraft Java Edition server software built on top of Minestom
+
+REM Check if Java is installed
+java -version >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Java is not installed. Please install Java 25 or higher.
+    pause
+    exit 1
+)
+
+REM Check if server jar exists
+set JAR_FILE=
+for %%f in (minestom4fun-server-*-shaded.jar) do set JAR_FILE=%%f
+
+IF "%JAR_FILE%"=="" (
+    echo Server jar not found.
+    echo You can download the file from https://github.com/Kanelucky/Minestom4fun/releases
+    pause
+    exit 1
+)
+
+set JAVA_CMD="java"
+
+%JAVA_CMD% -Xmx3072M -Xms1024M ^
+    --enable-native-access=ALL-UNNAMED ^
+    -XX:+UseZGC ^
+    -XX:+ZGenerational ^
+    -XX:+UseStringDeduplication ^
+    -jar "%JAR_FILE%" nogui
+
+pause
